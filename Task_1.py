0b21219b981f8866957bbf0922e9fab4557d0e4d
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2020-07-01 17:16:55 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,25 +1,10 @@
-"""
-1. Реализовать класс Matrix (матрица).
-Обеспечить перегрузку конструктора класса (метод __init__()),
-который должен принимать данные (список списков) для формирования матрицы.
-Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
-Примеры матриц вы найдете в методичке.
-Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
-Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов класса Matrix (двух матриц).
- Результатом сложения должна быть новая матрица.
-Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем с
- первым элементом первой строки второй матрицы и т.д.
-"""
-
-
-
 class Matrix:
     def __init__(self, list_of_lists):
 #        self.list_of_lists = [[]]
         self.list_of_lists = list_of_lists
 
     def __str__(self):
-        pass
+        return '\n'.join('\t'.join(map(str, row)) for row in self.list_of_lists)
 
     def __add__(self, other):
         other = Matrix(other)
@@ -27,7 +12,7 @@ class Matrix:
         sum_matrix = []
         final = []
         for i in range(len(self.list_of_lists)):
-            for j in range(len(self.list_of_lists[i])):
+            for j in range(len(self.list_of_lists[0])):
                 sum_el = self.list_of_lists[i][j] + other[i][j]
                 sum_matrix.append(sum_el)
                 print(sum_el)
@@ -39,9 +24,9 @@ class Matrix:
 
 
 my_matrix_1 = Matrix([[1, 2], [3, 4], [5, 6]])
-my_matrix_2 = Matrix([7, 8], [9, 10], [11, 12])
+my_matrix_2 = Matrix([[7, 8], [9, 10], [11, 12]])
 print(my_matrix_1)
+print()
 print(my_matrix_2)
 
-print(my_matrix_1 + my_matrix_2)
-
+#print(my_matrix_1 + my_matrix_2)
