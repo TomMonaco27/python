a4e22a10095384df6f15f029aaa64c7d44a0d060
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2020-07-02 09:41:00 +0300

    Update Task_2_2.py

--- Task_2_2.py
+++ Task_2_2.py
@@ -28,9 +28,9 @@ class Clothes(ABC):
     def consumption_fabric(self, size, height):
         pass
 
-    def total_fabric(self, size, height):
-        self.coat = size / self.const_coat_6_5 + self.const_coat_0_5
-        self.suit = height / self.const_suit_2 + self.const_suit_0_3
+    def total_fabric(self):
+        self.coat = self.size / self.const_coat_6_5 + self.const_coat_0_5
+        self.suit = self.height / self.const_suit_2 + self.const_suit_0_3
         self.total = self.coat + self.suit
         return self.total
 
